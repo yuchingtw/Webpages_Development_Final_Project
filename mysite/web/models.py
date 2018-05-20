371d0e3d9b3f0f28a0b42d700f43b195f51b7473
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-05-20 22:59:23 +0800

    uuid change

--- mysite/web/models.py
+++ mysite/web/models.py
@@ -1,18 +1,24 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 
 class Comment(models.Model):
-    ucid = models.UUIDField(primary_key=True, editable=False)
+    ucid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     owner_id = models.CharField(max_length=50)
     publish_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Video(models.Model):
-    uvid = models.UUIDField(primary_key=True, editable=False)
+    uvid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     classify = models.CharField(max_length=20)
@@ -25,9 +31,13 @@ class Video(models.Model):
     owner_id = models.CharField(max_length=50)  # Account username
     comments = models.ForeignKey(Comment, null=True, blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
-    upid = models.UUIDField(primary_key=True, editable=False)
+    upid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     classify = models.CharField(max_length=20)
@@ -39,12 +49,16 @@ class Post(models.Model):
     owner_id = models.CharField(max_length=50)  # Account username
     comments = models.ForeignKey(Comment, null=True, blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Account(models.Model):
     """
     這個是帳號, username ,password_sha256, name, intro
     """
-    uuid = models.UUIDField(primary_key=True, editable=False)
+    uuid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
     username = models.CharField(max_length=50)
     password_sha256 = models.CharField(max_length=64)
     name = models.CharField(max_length=20)
@@ -57,3 +71,6 @@ class Account(models.Model):
         max_digits=10, decimal_places=0, default=0)  # unit:second
     videos = models.ForeignKey(Video, null=True, blank=True)
     posts = models.ForeignKey(Post, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.uuid) + self.username
