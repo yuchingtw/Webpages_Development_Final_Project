f4105404c47ef13a3f04208a47b6084a216fceb0
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-05-20 13:59:32 +0800

    login_page

---
+++ mysite/web/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from web import views
+
+urlpatterns = [
+    url(r'login/', views.login, name="login_page"),
+    url(r'login_check/', views.login_check, name='login_check'),
+]
--- mysite/web/views.py
+++ mysite/web/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from web.models import *
+import hashlib
 # Create your views here.
+
+
+def login(request):
+    """
+    登入頁面顯示 跳轉首頁
+    """
+    if 'user' in request.COOKIES:
+        return HttpResponse('checked user your cookies is' + request.COOKIES['user'])
+    return render(request, "login/login.html")
+
+
+def login_check(request):
+    '''
+    檢查使用者
+    '''
+    username = request.POST.get("username")
+    password = request.POST.get("password").encode(encoding='utf-8')
+    accounts = Account.objects.all()
+    if username in accounts.username:
+        password_hash256 = hashlib.sha256(password).hexdigest()
+    else:
+        return HttpResponse('Login failed')
+
+    mycontext = {'A': 'B', 'hash': password_hash256}
+    response = render(request, "login/login.html", mycontext)
+    #response.set_cookie('user', value="username")
+    return response
