8f54f82340ce9a6535baf646a81b8e505ba722e7
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-05-21 01:07:59 +0800

    註冊hash 登入cookies

--- mysite/web/urls.py
+++ mysite/web/urls.py
@@ -4,6 +4,6 @@ from web import views
 
 urlpatterns = [
     url(r'login/', views.login, name="login_page"),
-    url(r'register/',views.register,name="register_page"),
+    url(r'register/', views.register, name="register_page"),
     url(r'login_check/', views.login_check, name='login_check'),
 ]
--- mysite/web/views.py
+++ mysite/web/views.py
@@ -1,51 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from web.models import *
 import hashlib
 # Create your views here.
 
+# 這是給cookies用的secret
+secret = 'secret'
 
-#login
+
+# login
 def login(request):
     """
     登入頁面顯示 跳轉首頁 這也還沒寫完
     """
-    if 'user' in request.COOKIES:
-        return HttpResponse('checked user your cookies is' + request.COOKIES['user'])
+    try:
+        accounts = Account.objects.all()
+        for account in accounts:
+            if hashlib.sha256((account.username + secret).encode('utf-8')).hexdigest() == request.COOKIES['username']:
+                return HttpResponse('log in as ' + account.username)  # 跳轉  還沒寫
+    except KeyError as e:
+        pass
+
+    # 沒有登入救回傳正常的登入頁面
     return render(request, "login/login.html")
 
 
 def login_check(request):
     '''
-    檢查使用者 還沒寫完拉
+    檢查使用者 確認正確使用者
     '''
     username = request.POST.get("username")
-    password = request.POST.get("password").encode(encoding='utf-8')
-    accounts = Account.objects.all()
-    if username in accounts.username:
-        password_hash256 = hashlib.sha256(password).hexdigest()
+    password_sha256 = hashlib.sha256(request.POST.get(
+        "password").encode(encoding='utf-8')).hexdigest()
+    account = get_object_or_404(Account, username=username)
+    if account.password_sha256 == password_sha256:
+        response = HttpResponse("login success as " + username)
+        response.set_cookie('username', value=hashlib.sha256((
+            username + secret).encode('utf-8')).hexdigest())
+        return response
+    return HttpResponse('LOGIN FAILD')
 
-    else:
-        return HttpResponse('Login failed')
+# rigister
 
-    mycontext = {'A': 'B', 'hash': password_hash256}
-    response = render(request, "login/login.html", mycontext)
-    #response.set_cookie(hash('user'+'secret'), value="username")
-    return response
 
-#rigister
 def register(request):
     '''
     密碼還沒做hash設定
     '''
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.POST.get("username")
-        password = request.POST.get("password").encode(encoding='utf-8')
-        name=request.POST.get("name")
-        if(username==''or password=='' or name==''  ):
+        password_sha256 = hashlib.sha256(request.POST.get(
+            "password").encode(encoding='utf-8')).hexdigest()
+        name = request.POST.get("name")
+        if(username == ''or password_sha256 == '' or name == ''):
             return HttpResponse('欄位不能為空白')
-        Account.objects.create(username=username,password_sha256=password,name=name)
+        Account.objects.create(
+            username=username, password_sha256=password_sha256, name=name)
         return HttpResponse('註冊成功')
-        
-    return render(request,'register/register.html')
+
+    return render(request, 'register/register.html')
