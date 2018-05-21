d59dbf54f67686a8c93ee7988080ccec6c5bdd11
gj94gj94ao4 <gj94gj94ao4@gmail.com>
2018-05-21 20:47:39 +0800

    登出 和 註冊重複輸入密碼確認

--- mysite/web/views.py
+++ mysite/web/views.py
@@ -16,6 +16,8 @@ def login(request):
     登入頁面顯示 跳轉首頁 這也還沒寫完
     """
     try:
+        if request.COOKIES['username'] == 'anonymous':
+            pass
         accounts = Account.objects.all()
         for account in accounts:
             if hashlib.sha256((account.username + secret).encode('utf-8')).hexdigest() == request.COOKIES['username']:
@@ -25,6 +27,18 @@ def login(request):
 
     # 沒有登入救回傳正常的登入頁面
     return render(request, "login/login.html")
+
+
+def logout(request):
+    """
+    設定 cookeis username 為 anonymous
+    """
+    try:
+        response = HttpResponse('log out success')
+        response.set_cookie('username', value="anonymous")
+        return response  # 跳轉  還沒寫
+    except KeyError:
+        pass
 
 
 def login_check(request):
@@ -42,26 +56,31 @@ def login_check(request):
         return response
     return HttpResponse('LOGIN FAILD')
 
-# rigister
-
 
 def register(request):
     '''
     密碼還沒做hash設定
     '''
+
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password_sha256 = hashlib.sha256(request.POST.get(
-            "password").encode(encoding='utf-8')).hexdigest()
-        name = request.POST.get("name")
-        if(username == ''or password_sha256 == '' or name == ''):
-            return HttpResponse('欄位不能為空白')
         try:
+            username = request.POST.get("username")
+            password_sha256 = hashlib.sha256(request.POST.get(
+                "password").encode(encoding='utf-8')).hexdigest()
+            password_sha256_repeat = hashlib.sha256(request.POST.get(
+                "password_repeat").encode(encoding='utf-8')).hexdigest()
+            name = request.POST.get("name")
+
+            if password_sha256 != password_sha256_repeat:
+                raise Exception("密碼前後不一致")
+            if username == '' or password_sha256 == '' or name == '':
+                raise Exception("輸入空白值")
             Account.objects.create(
                 username=username, password_sha256=password_sha256, name=name)
         except IntegrityError:
             return HttpResponse('存在相同用戶名')
-
+        except Exception as e:
+            return HttpResponse(e)
         return HttpResponse('註冊成功')
 
     return render(request, 'register/register.html')
