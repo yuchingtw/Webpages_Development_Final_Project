b73c72825950cfc24f73069bc00920af80dd577a
YuChing Chen <yuchingchen.tw@gmail.com>
2018-06-09 15:25:57 +0800

    change index url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls.static import static
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^web', include("web.urls")),
+    url(r'^', include("web.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/web/urls.py
+++ mysite/web/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import url
 from web import views
 
 urlpatterns = [
+    url(r'^$', views.index, name="index"),
     url(r'login/', views.login, name="login_page"),
     url(r'logout/', views.logout, name="logout"),
     url(r'register/', views.register, name="register_page"),
     url(r'logrequired/', views.login_require_page, name="logrequired"),
     url(r'index/', views.index, name="index"),
-    url(r'create_code/',views.create_code_img,name="create_code"),
+    url(r'create_code/', views.create_code_img, name="create_code"),
     url(r'uploadvideo/', views.upload_video),
     url(r'postShow/', views.post_show, name="post_show"),
     url(r'postList/', views.post_list, name="post_list"),
